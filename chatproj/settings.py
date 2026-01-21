47e22e6f3692529d6ec2c894f5f2bb005581e744
Dali Zammit <chatti.zammit.mohamed.ali@gmail.com>
2026-01-22 00:10:00 +0100

    Fix errors

--- chatproj/settings.py
+++ chatproj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 DEBUG = int(os.environ.get('DEBUG', default=1))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+#DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
